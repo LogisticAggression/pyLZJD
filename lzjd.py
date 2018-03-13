5260b495cb04bc2f82860f8ec2e50d0db130c989
Rob Brandon <brandon_robert@bah.com>
2018-03-13 09:32:11 -0400

    Cleaned up comments

--- lzjd.py
+++ lzjd.py
@@ -1,8 +1,7 @@
 """
 Python implementation of Lempel Ziv Jaccard Distance
 
-Uses the implementation Murmurhash3 written by Hajime Senuma
-located at https://github.com/hajimes/mmh3, not the version which installs with pip
+
 """
 
 import mmh3
