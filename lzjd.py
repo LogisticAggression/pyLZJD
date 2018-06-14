2bf1ae4762a805c8e2cc011b51a6537e696d91b7
phreaksh0 <phreaksh0@protonmail.com>
2018-06-14 15:03:43 -0400

    Added more signature handling

--- lzjd.py
+++ lzjd.py
@@ -23,14 +23,36 @@ class LZJD_Hashifier:
         return lzset
 
     def get_min_hashes(self, lzset):
-        return heapq.nsmallest(self.k, [mmh3.hash(x, signed=False) for x in lzset])
+        return set(heapq.nsmallest(self.k, [mmh3.hash(x, signed=False) for x in lzset]))
 
-    def get_signature_from_buffer(self, buffer):
-        return ''.join(['{:02x}'.format(x).zfill(8) for x in self.get_min_hashes(self.build_lz_set(buffer))])
+    def get_signature(self, min_hashes):
+        return ''.join(['{:02x}'.format(x).zfill(8) for x in min_hashes])
+
+    def get_min_hashes_from_signature(self, signature):
+        return set([int(signature[i:i+8], 16) for i in range(0, len(signature), 8)])
 
 
 def get_lzjd_dist(buff1, buff2):
     hasher = LZJD_Hashifier()
-    setA = hasher.get_signature_from_buffer(buff1)
-    setB = hasher.get_signature_from_buffer(buff2)
+    setA = hasher.get_min_hashes(buff1)
+    setB = hasher.get_min_hashes(buff2)
     return len(setA.intersection(setB)) / len(setA.union(setB))
+
+def compare_file_to_signature(signature, filename):
+    hasher = LZJD_Hashifier()
+    setA = hasher.get_min_hashes_from_signature(signature)
+    setB = hasher.get_min_hashes(open(filename, "rb"))
+    return len(setA.intersection(setB)) / len(setA.union(setB))
+
+
+
+if __name__ == "__main__":
+    file1 = "c://Windows/System32/calc.exe"
+    file2 = "c://Windows/System32/cmd.exe"
+    hasher = LZJD_Hashifier()
+    test = hasher.get_min_hashes(open(file1, "rb"))
+    print(test)
+    test2 = hasher.get_signature(hasher.get_min_hashes(open(file1, "rb")))
+    print(test2)
+    print(compare_file_to_signature(test2, file1))
+    print(compare_file_to_signature(test2, file2))
